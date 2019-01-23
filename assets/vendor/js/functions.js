a884c0f80f6b76530a566338a0f51da0a5fadd0a
Robert Molnar <DevRob@users.noreply.github.com>
2019-01-23 12:39:05 +0000

    Initial upload

---
+++ assets/vendor/js/functions.js
@@ -0,0 +1,31 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  // This function changes the display property(CSS) of the .slider-content element to show the right slide.
+  var i;
+  var slides = document.getElementsByClassName("slider-content");
+  if (n > slides.length) {slideIndex = 1;}
+  if (n < 1) {slideIndex = slides.length;}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
+
+function myMap() {
+  // Google Map init function
+  var mapProp = {
+    center:new google.maps.LatLng(51.508742,-0.120850),
+    zoom:5,
+  };
+  var map = new google.maps.Map(document.getElementById("map"),mapProp);
+}
